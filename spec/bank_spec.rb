e38faa0cb3e77a8afc68eb5927e9a176240899c7
Sherif Shendidy <sherif@shendidy.com>
2019-03-11 12:23:34 +0000

    :white_check_mark: test: check if user can use withdraw function

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,5 +5,8 @@ describe Bank do
     it 'check responce to deposit' do
       is_expected.to respond_to(:deposit)
     end
+    it 'check responce to withdraw' do
+      is_expected.to respond_to(:withdraw)
+    end
   end
 end
