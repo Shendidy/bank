e4094a1cfb8651480b2704dfc578abf43c1c8315
Sherif Shendidy <sherif@shendidy.com>
2019-03-11 12:16:07 +0000

    :art: test: fixed test style inline with rubocop

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,8 +1,8 @@
 require 'bank'
 
 describe Bank do
-  context "Initial context" do
-    it "check responce to deposit" do
+  context 'Initial context' do
+    it 'check responce to deposit' do
       is_expected.to respond_to(:deposit)
     end
   end
