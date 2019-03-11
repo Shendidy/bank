39570b62066483149c0f3b73ccf513c736ef0d38
Sherif Shendidy <sherif@shendidy.com>
2019-03-11 14:00:45 +0000

    :white_check_mark: test: check if user can withdraw from account

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,7 +1,7 @@
 require 'bank'
 
 describe Bank do
-  context 'Initial context' do
+  context 'initial context' do
     it 'check responce to deposit' do
       is_expected.to respond_to(:deposit)
     end
@@ -12,7 +12,9 @@ describe Bank do
       is_expected.to respond_to(:balance)
     end
   end
+end
 
+describe 'Bank accounts' do
   context 'Using bank account' do
     it 'user can check current balance' do
       account = Bank.new
@@ -22,6 +24,12 @@ describe Bank do
       account = Bank.new
       account.deposit(10)
       expect(account.balance).to eq 10
+    end
+    it 'user can withdraw from it' do
+      account = Bank.new
+      account.deposit(10)
+      account.withdraw(5)
+      expect(account.balance).to eq 5
     end
   end
 end
